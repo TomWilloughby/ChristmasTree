b06750c2a9e709b2ab1441e19254ad49663d23a3
Tom Willoughby <t.willoughby@htempest.co.uk>
2026-01-06 15:13:34 +0000

    Add a background task to manage the line animation

--- ChristmasTreeUI/Program.cs
+++ ChristmasTreeUI/Program.cs
@@ -1,4 +1,28 @@
 ﻿using ChristmasTreeUI;
 
+var signal = new AutoResetEvent(false);
+
 using var win = new CustomWindow("Hello World");
+var line = new Line(50, 50, 250, 250, 10_000);
+
+var task = Task.Run(() =>
+{
+    CustomWindow.OnPaint += CustomWindow_OnPaint;
+    line.StartAnimation();
+    win.Repaint();
+    signal.WaitOne();
+    CustomWindow.OnPaint -= CustomWindow_OnPaint;
+});
+
+void CustomWindow_OnPaint(IntPtr hdc)
+{
+    line.Draw(hdc);
+
+    if (line.IsAnimating)
+    {
+        win.Repaint();
+    }
+}
+
 win.Loop();
+signal.Set();
