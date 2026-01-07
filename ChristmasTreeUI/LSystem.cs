3b9787d3d4df2ae8512a5a53bb373586d137ea55
Tom Willoughby <t.willoughby@htempest.co.uk>
2026-01-07 16:02:37 +0000

    Add a class to calculate generations in an L-System

---
+++ ChristmasTreeUI/LSystem.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace ChristmasTreeUI;
+
+internal class LSystem(string axiom, Dictionary<char, string> rules)
+{
+    private string current = axiom;
+    public string Value => current;
+
+    public void Iterate()
+    {
+        StringBuilder next = new();
+        foreach (char c in current)
+        {
+            if (rules.TryGetValue(c, out string? value))
+            {
+                next.Append(value);
+            }
+            else
+            {
+                next.Append(c);
+            }
+        }
+
+        current = next.ToString();
+    }
+}
