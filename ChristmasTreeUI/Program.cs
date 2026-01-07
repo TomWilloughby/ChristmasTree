d0a8b4f0bd86d807f3bc00d50f4fc351a9cc2961
Tom Willoughby <t.willoughby@htempest.co.uk>
2026-01-07 16:04:55 +0000

    Update main program to render the L-System instead of drawing random lines

--- ChristmasTreeUI/Program.cs
+++ ChristmasTreeUI/Program.cs
@@ -1,23 +1,80 @@
 ﻿using ChristmasTreeUI;
 
 var rnd = new Random();
-var signal = new AutoResetEvent(false);
+
+var complete = false;
+var completeLock = new object();
+
+var system = new LSystem("0", new Dictionary<char, string>() { { '0', "1[0]0" }, { '1', "11" } });
 
 using var win = new CustomWindow("Hello World");
 
-var allLines = new List<Line>();
-var currentLine = new Line(50, 50, 250, 250, 5_000);
-allLines.Add(currentLine);
+List<Line> allLines = [];
 
 var task = Task.Run(() =>
 {
     try
     {
         CustomWindow.OnPaint += CustomWindow_OnPaint;
-        currentLine.OnAnimationComplete += Line_OnAnimationComplete;
-        currentLine.StartAnimation();
-        win.Repaint();
-        signal.WaitOne();
+
+        while (true)
+        {
+            lock (completeLock)
+            {
+                if (complete)
+                {
+                    break;
+                }
+            }
+
+            int x = 500, y = 0, angle = 0;
+            var positions = new Stack<(int, int, int)>();
+            allLines.Clear();
+            Line? nextLine = null;
+
+            foreach (char symbol in system.Value)
+            {
+                nextLine = null;
+                switch (symbol)
+                {
+                    case '0':
+                        nextLine = Draw(x, y, angle, true); // Draw leaf
+                        break;
+                    case '1':
+                        nextLine = Draw(x, y, angle, false); // Draw line
+                        break;
+                    case '[':
+                        // push position and angle, turn left 45 degrees
+                        positions.Push((x, y, angle));
+                        angle -= 45;
+                        break;
+                    case ']':
+                        // pop position and angle, turn right 45 degrees
+                        (x, y, angle) = positions.Pop();
+                        angle += 45;
+                        break;
+                }
+
+                if (angle < 0)
+                {
+                    angle = 315;
+                }
+
+                if (angle >= 360)
+                {
+                    angle = 0;
+                }
+
+                if (nextLine != null)
+                {
+                    x = nextLine.EndX;
+                    y = nextLine.EndY;
+                }
+            }
+
+            system.Iterate();
+        }
+
         CustomWindow.OnPaint -= CustomWindow_OnPaint;
     }
     catch (Exception ex)
@@ -27,22 +84,50 @@ var task = Task.Run(() =>
     }
 });
 
-void Line_OnAnimationComplete()
+Line Draw(int startX, int startY, int angle, bool isLeaf)
 {
-    try
+    var lineLength = isLeaf ? 10 : 50;
+    var (endX, endY) = CalculateEndPosition(startX, startX, angle, lineLength);
+    int r = 0, g = 0, b = 0;
+
+    if (isLeaf)
     {
-        currentLine.OnAnimationComplete -= Line_OnAnimationComplete;
-        var replacementLine = new Line(currentLine.EndX, currentLine.EndY, rnd.Next(0, 500), rnd.Next(0, 500), 5_000);
-        replacementLine.OnAnimationComplete += Line_OnAnimationComplete;
-        replacementLine.StartAnimation();
-        allLines.Add(replacementLine);
-        currentLine = replacementLine;
+        g = 255;
     }
-    catch (Exception ex)
+    else
     {
-        Console.WriteLine(ex.ToString());
-        throw;
+        r = 105;
+        g = 80;
     }
+
+    var line = new Line(startX, startY, endX, endY, 2_000, r, g, b);
+    allLines.Add(line);
+    line.StartAnimation();
+    win.Repaint();
+    line.WaitForAnimationEnd();
+    return line;
+}
+
+
+/// Calculate the end position of a line starting at x,y with a given angle and length.
+/// It's assumed angle is a multiple of 45 degrees.
+(int, int) CalculateEndPosition(int x, int y, int angle, int lineLength)
+{
+    double rootTwo = Math.Sqrt(2);
+    var angledLength = (int)Math.Round(lineLength / rootTwo);
+
+    return angle switch
+    {
+        0 => (x, y + lineLength),// line go up
+        45 => (x + angledLength, y + angledLength),// line go up & right
+        90 => (x + lineLength, y),// line go right
+        135 => (x + angledLength, y - angledLength),// line go down & right
+        180 => (x, y - lineLength),// line go down
+        225 => (x - angledLength, y - angledLength),// line go down & left
+        270 => (x - lineLength, y),// line go left
+        315 => (x - angledLength, y + angledLength),// line go up & left
+        _ => throw new NotImplementedException($"Angles that aren't multiples of 45 degrees aren't supported (trying to calculate line for angle {angle})."),
+    };
 }
 
 void CustomWindow_OnPaint(IntPtr hdc)
@@ -61,7 +146,7 @@ void CustomWindow_OnPaint(IntPtr hdc)
         }
     }
 
-    if (currentLine.IsAnimating)
+    if (lines.Any(line => line.IsAnimating))
     {
         win.Repaint();
     }
@@ -76,7 +161,11 @@ catch (Exception ex)
     Console.WriteLine(ex.ToString());
     throw;
 }
-signal.Set();
+
+lock (completeLock)
+{
+    complete = true;
+}
 
 /*
 0: draw a line segment ending in a leaf
